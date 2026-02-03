880993bb0b8fc82ec855d6c23df264b92ab0083e
SonXeeZ <sonxe@hotmail.hu>
2026-02-03 23:50:07 +0100

    Updated pdf paths to properly work on github pages

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,8 +5,8 @@ import heroBg from '/hero-bg.jpg';
 const Hero = () => {
 
   const [selectedLanguage, setSelectedLanguage] = useState('en')
-  const cvHun = '/Földesi_Krisztofer_CV_HUN.pdf'
-  const cvEng = '/Krisztofer_Földesi_CV_ENG.pdf'
+  const cvHun = `${import.meta.env.BASE_URL}Földesi_Krisztofer_CV_HUN.pdf`;
+  const cvEng = `${import.meta.env.BASE_URL}Krisztofer_Földesi_CV_ENG.pdf`;
 
   const handleLanguageChange = (e) => {
     setSelectedLanguage(e.target.value);
